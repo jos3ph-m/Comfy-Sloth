a3356e210ad5b13ae9bd05cd3629a4a476b4fc66
Joseph M <jmedina1985@hotmail.com>
2021-06-22 22:09:40 -0700

    Getting set up

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Navbar, Sidebar, Footer } from './components';
 
-import Testing from './Testing';
-
 function App() {
   return (
     <div>
       <h4>comfy sloth starter</h4>
-      <Testing />
     </div>
   );
 }
