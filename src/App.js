204ef3e8bd94f439697bf030f95c9298affe388c
Joseph M <jmedina1985@hotmail.com>
2021-06-22 22:27:54 -0700

    Router set up

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Navbar, Sidebar, Footer } from './components';
 
+import { Home } from './pages';
+
 function App() {
   return (
     <div>
