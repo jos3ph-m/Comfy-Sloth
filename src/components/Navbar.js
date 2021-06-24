348487b79aa0bf6cbed44a38ac2f3d0ab3456e07
Joseph M <jmedina1985@hotmail.com>
2021-06-24 15:36:47 -0700

    Working on Nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,9 @@ import { useUserContext } from '../context/user_context';
 const Nav = () => {
   return (
     <NavContainer>
-      <div className="nav-center"></div>
+      <div className="nav-center">
+        <div className="nav-header"></div>
+      </div>
     </NavContainer>
   );
 };
