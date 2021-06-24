b440fcdfc71c82b0b3d04ecedca99d4e0a9992ee
Joseph M <jmedina1985@hotmail.com>
2021-06-24 15:50:45 -0700

    Cart buttons

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import { FaShoppingCart, FaUserMinus, FaUserPlus } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
-import { useProductsContext } from '../context/products_context'
-import { useCartContext } from '../context/cart_context'
-import { useUserContext } from '../context/user_context'
+import React from 'react';
+import { FaShoppingCart, FaUserMinus, FaUserPlus } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { useProductsContext } from '../context/products_context';
+import { useCartContext } from '../context/cart_context';
+import { useUserContext } from '../context/user_context';
 
 const CartButtons = () => {
-  return <h4>cart buttons </h4>
-}
+  return <Wrapper className="cart-btn-wrapper">cart buttons </Wrapper>;
+};
 
 const Wrapper = styled.div`
   display: grid;
@@ -62,5 +62,5 @@ const Wrapper = styled.div`
       margin-left: 5px;
     }
   }
-`
-export default CartButtons
+`;
+export default CartButtons;
