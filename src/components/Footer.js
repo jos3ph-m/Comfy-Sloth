ce1f03cca6a0ab02b349059e1cd5fe07a22d14a0
Joseph M <jmedina1985@hotmail.com>
2021-06-25 10:46:30 -0700

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 const Footer = () => {
-  return <h4>footer</h4>
-}
+  return <Wrapper>footer</Wrapper>;
+};
 
 const Wrapper = styled.footer`
   height: 5rem;
@@ -26,6 +26,6 @@ const Wrapper = styled.footer`
   @media (min-width: 776px) {
     flex-direction: row;
   }
-`
+`;
 
-export default Footer
+export default Footer;
