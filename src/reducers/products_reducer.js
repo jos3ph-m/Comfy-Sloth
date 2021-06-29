8bf7faed20da00ec5dee1dbe86f26e9b92a384b0
Joseph M <jmedina1985@hotmail.com>
2021-06-28 21:13:29 -0700

    Products context setup

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -11,7 +11,6 @@ import {
 
 const products_reducer = (state, action) => {
   if (action.type === 'SIDEBAR_OPEN') {
-    console.log(action);
   }
   return state;
   throw new Error(`No Matching "${action.type}" - action type`);
