1b01b809bdef5efe3e77f8b9bbbaa44f99884ce0
Joseph M <jmedina1985@hotmail.com>
2021-06-29 18:35:00 -0700

    Sidebar toggle

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -6,6 +6,8 @@ const ErrorPage = () => {
     <Wrapper className="page-100">
       <section>
         <h1>404</h1>
+        <h3>Sorry, the page you tried cannot be found</h3>
+        <Link></Link>
       </section>
     </Wrapper>
   );
