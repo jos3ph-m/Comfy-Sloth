4294ce8301742dddea7b98d4d933caa708a06ecd
Joseph M <jmedina1985@hotmail.com>
2021-06-29 18:10:42 -0700

    Sidebar toggle

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -11,7 +11,7 @@ const CartButtons = () => {
 
   return (
     <Wrapper className="cart-btn-wrapper">
-      <Link to="/cart" className="cart-btn">
+      <Link to="/cart" className="cart-btn" onClick={closeSidebar}>
         Cart
         <span className="cart-container">
           <FaShoppingCart />
