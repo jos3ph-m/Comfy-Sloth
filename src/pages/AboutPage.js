6ffac4ea2349eb44a6376ea32e3e11ba7bd2eabd
Joseph M <jmedina1985@hotmail.com>
2021-06-30 17:26:28 -0700

    About and checkout pages

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -6,7 +6,7 @@ import aboutImg from '../assets/hero-bcg.jpeg';
 const AboutPage = () => {
   return (
     <main>
-      <PageHero title="about" />
+      <PageHero title="About" />
       <Wrapper className="page section section-center">
         <img src={aboutImg} alt="nice desk" />
         <article>
