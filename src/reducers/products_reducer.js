236082852e5cf3a48a2400760f049f89894a0437
Joseph M <jmedina1985@hotmail.com>
2021-07-03 14:34:01 -0700

    Fetch products

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -34,6 +34,10 @@ const products_reducer = (state, action) => {
     };
   }
 
+  if (action.type === GET_PRODUCTS_ERROR) {
+    return { ...state, products_loading: false, products_error: true };
+  }
+
   throw new Error(`No Matching "${action.type}" - action type`);
 };
 
