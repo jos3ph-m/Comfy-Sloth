caaefd4f3aae6dd8bb2c9f285e51437b0b1dbdf1
Joseph M <jmedina1985@hotmail.com>
2021-07-05 13:56:29 -0700

    Fetch single product

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -19,6 +19,9 @@ const initialState = {
   products_error: false,
   products: [],
   featured_products: [],
+  single_product_loading: false,
+  single_product_error: false,
+  single_product: {},
 };
 
 const ProductsContext = React.createContext();
@@ -43,6 +46,10 @@ export const ProductsProvider = ({ children }) => {
     } catch (error) {
       dispatch({ type: GET_PRODUCTS_ERROR });
     }
+  };
+
+  const fetchSingleProduct = async (url) => {
+    dispatch({ type: GET });
   };
 
   useEffect(() => {
