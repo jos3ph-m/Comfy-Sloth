bfcb1c153bbc74b79acd5a4b106bd95ed9409a66
Joseph M <jmedina1985@hotmail.com>
2021-07-06 20:53:15 -0700

    Single product loading and error

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -16,7 +16,7 @@ import { Link } from 'react-router-dom';
 
 const SingleProductPage = () => {
   const { id } = useParams();
-  const {single_product_loading: loading}
+  const {single_product_loading: loading, single_product_error: error}
   return <h4>single product page</h4>;
 };
 
