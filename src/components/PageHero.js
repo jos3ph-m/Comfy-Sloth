6b1e45655c1987bd3a9ba5056512b4f7eb2e07ab
Joseph M <jmedina1985@hotmail.com>
2021-07-07 20:21:20 -0700

    Base return

--- src/components/PageHero.js
+++ src/components/PageHero.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-const PageHero = ({ title }) => {
+const PageHero = ({ title, product }) => {
   return (
     <Wrapper>
       <div className="section-center">
