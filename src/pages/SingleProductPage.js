40a3401f0342cf43feeeeb74047c6bded07f34eb
Joseph M <jmedina1985@hotmail.com>
2021-07-07 20:24:05 -0700

    Base return

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -62,6 +62,7 @@ const SingleProductPage = () => {
         <Link to="/products" className="btn">
           back to products
         </Link>
+        <div className="product-center"></div>
       </div>
     </Wrapper>
   );
