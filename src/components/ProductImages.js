1a9ba2567dd05bc08502205077a812dfe23aa411
Joseph M <jmedina1985@hotmail.com>
2021-07-09 17:28:45 -0700

    Product images

--- src/components/ProductImages.js
+++ src/components/ProductImages.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
-const ProductImages = ({ images = [] }) => {
+const ProductImages = ({ images = [{ url: '' }] }) => {
   const [main, setMain] = useState(images[0]);
 
   return (
