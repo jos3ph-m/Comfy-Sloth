631f12a7b76319e1358f72eccfa70285e476df65
Joseph M <jmedina1985@hotmail.com>
2021-07-09 17:59:28 -0700

    Stars

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -6,7 +6,52 @@ const Stars = ({ stars, reviews }) => {
     <Wrapper>
       <div className="stars">
         {/* start stars */}
-        <span>{stars >= 1 ? <BsStarFill /> : null}</span>
+        <span>
+          {stars >= 1 ? (
+            <BsStarFill />
+          ) : stars >= 0.5 ? (
+            <BsStarHalf />
+          ) : (
+            <BsStar />
+          )}
+        </span>
+        <span>
+          {stars >= 2 ? (
+            <BsStarFill />
+          ) : stars >= 1.5 ? (
+            <BsStarHalf />
+          ) : (
+            <BsStar />
+          )}
+        </span>
+        <span>
+          {stars >= 3 ? (
+            <BsStarFill />
+          ) : stars >= 2.5 ? (
+            <BsStarHalf />
+          ) : (
+            <BsStar />
+          )}
+        </span>
+        <span>
+          {stars >= 4 ? (
+            <BsStarFill />
+          ) : stars >= 3.5 ? (
+            <BsStarHalf />
+          ) : (
+            <BsStar />
+          )}
+        </span>
+        <span>
+          {stars >= 5 ? (
+            <BsStarFill />
+          ) : stars >= 4.5 ? (
+            <BsStarHalf />
+          ) : (
+            <BsStar />
+          )}
+        </span>
+
         {/* end stars */}
       </div>
       <p className="reviews">({reviews} customer reviews)</p>
