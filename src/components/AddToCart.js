f4673e4a49fd447e79b1a924d328467e84c564a0
Joseph M <jmedina1985@hotmail.com>
2021-07-10 22:56:01 -0700

    Add to cart component

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -25,7 +25,7 @@ const AddToCart = ({ product }) => {
                 }`}
                 onClick={() => setMainColor(color)}
               >
-                {mainColor === color}
+                {mainColor === color ? <FaCheck />}
               </button>
             );
           })}
