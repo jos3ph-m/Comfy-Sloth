e2b8b77d0c9411401841ee822aaa3b78b2279f32
Joseph M <jmedina1985@hotmail.com>
2021-07-12 15:36:31 -0700

    Filter context setup

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -22,6 +22,11 @@ const FilterContext = React.createContext();
 export const FilterProvider = ({ children }) => {
   const { products } = useProductsContext();
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    dispatch({ type: LOAD_PRODUCTS, payload: products });
+  }, [products]);
+
   return (
     <FilterContext.Provider value="filter context">
       {children}
