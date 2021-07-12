a262996dc92b69151b76cadaab696033fdee8969
Joseph M <jmedina1985@hotmail.com>
2021-07-11 21:54:29 -0700

    Add to cart component

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -9,7 +9,7 @@ const AddToCart = ({ product }) => {
   const { id, stock, colors } = product;
 
   const [mainColor, setMainColor] = useState(colors[0]);
-
+  const [amount, setAmount] = useState(1);
   return (
     <Wrapper>
       <div className="colors">
