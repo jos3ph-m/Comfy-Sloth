2d9dadcb03721981d1316ca042f37860791e691b
Joseph M <jmedina1985@hotmail.com>
2021-07-12 21:39:34 -0700

    Grid view

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import { useFilterContext } from '../context/filter_context'
-import GridView from './GridView'
-import ListView from './ListView'
+import React from 'react';
+import { useFilterContext } from '../context/filter_context';
+import GridView from './GridView';
+import ListView from './ListView';
 
 const ProductList = () => {
-  return <h4>product list</h4>
-}
+  return <GridView>product list</GridView>;
+};
 
-export default ProductList
+export default ProductList;
