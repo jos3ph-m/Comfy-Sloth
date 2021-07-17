79acf17fc64c7b621949eaed2ad239ecbec428e5
Joseph M <jmedina1985@hotmail.com>
2021-07-16 17:38:41 -0700

    sort functionality

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -30,7 +30,7 @@ const filter_reducer = (state, action) => {
     const { sort, filtered_products } = state;
     let tempProducts = [...filtered_products];
     if (sort === 'price-lowest') {
-      console.log('price-lowest');
+      tempProducts = tempProducts.sort((a, b) => {});
     }
     if (sort === 'price-highest') {
       console.log('price-highest');
