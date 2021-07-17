cd4c62d497703a8893099bd07a4319e5610a988d
Joseph M <jmedina1985@hotmail.com>
2021-07-17 16:23:34 -0700

    Filters default value

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -12,6 +12,7 @@ import {
 const filter_reducer = (state, action) => {
   if (action.type === LOAD_PRODUCTS) {
     let maxPrice = action.payload.map((p) => p.price);
+    maxPrice = Math.max(...maxPrice);
     return {
       ...state,
       all_products: [...action.payload],
