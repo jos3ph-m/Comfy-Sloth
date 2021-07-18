6a4de4e03dc2df75949f48e565ad2f52c28620ae
Joseph M <jmedina1985@hotmail.com>
2021-07-17 17:07:26 -0700

    Filters default value

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -17,7 +17,7 @@ const filter_reducer = (state, action) => {
       ...state,
       all_products: [...action.payload],
       filtered_products: [...action.payload],
-      filters: {},
+      filters: { ...state.filters, max_price: maxPrice, price: maxPrice },
     };
   }
   if (action.type === SET_GRIDVIEW) {
