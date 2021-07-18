0cd942da4c871f5d30367c775739fc9970a3de89
Joseph M <jmedina1985@hotmail.com>
2021-07-17 17:06:54 -0700

    Filters default value

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -17,6 +17,7 @@ const filter_reducer = (state, action) => {
       ...state,
       all_products: [...action.payload],
       filtered_products: [...action.payload],
+      filters: {},
     };
   }
   if (action.type === SET_GRIDVIEW) {
