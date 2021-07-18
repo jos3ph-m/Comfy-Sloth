d5246ff860722be562d9cfc505eee25b63c57b67
Joseph M <jmedina1985@hotmail.com>
2021-07-17 20:46:29 -0700

    Filters default value

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import styled from 'styled-components'
-import { useFilterContext } from '../context/filter_context'
-import { getUniqueValues, formatPrice } from '../utils/helpers'
-import { FaCheck } from 'react-icons/fa'
+import React from 'react';
+import styled from 'styled-components';
+import { useFilterContext } from '../context/filter_context';
+import { getUniqueValues, formatPrice } from '../utils/helpers';
+import { FaCheck } from 'react-icons/fa';
 
 const Filters = () => {
-  return <h4>filters</h4>
-}
+  const {} = useFilterContext();
+  return <Wrapper>filters</Wrapper>;
+};
 
 const Wrapper = styled.section`
   .form-control {
@@ -105,6 +106,6 @@ const Wrapper = styled.section`
       top: 1rem;
     }
   }
-`
+`;
 
-export default Filters
+export default Filters;
