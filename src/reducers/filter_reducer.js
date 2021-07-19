c9cc2e628af552926e5cfaf6aa2ad3e1edf1f77b
Joseph M <jmedina1985@hotmail.com>
2021-07-18 22:03:32 -0700

    Filters default value

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -52,7 +52,7 @@ const filter_reducer = (state, action) => {
   }
   if (action.type === UPDATE_FILTERS) {
     const { name, value } = action.payload;
-    return { ...state, filters: { ...state.filters } };
+    return { ...state, filters: { ...state.filters, [name]: value } };
   }
   throw new Error(`No Matching "${action.type}" - action type`);
 };
