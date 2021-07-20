e8f2cff7c1b9cc9072d935873f6c5b9ecb9168c6
Joseph M <jmedina1985@hotmail.com>
2021-07-19 17:08:29 -0700

    Filter categories

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -37,7 +37,11 @@ const Filters = () => {
             <h5>category</h5>
             <div>
               {categories.map((c, index) => {
-                return <button key={index}>{c}</button>;
+                return (
+                  <button key={index} onClick={updateFilters}>
+                    {c}
+                  </button>
+                );
               })}
             </div>
           </div>
