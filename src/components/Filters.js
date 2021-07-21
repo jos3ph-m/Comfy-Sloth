bdac812b5dcdd960893021977cdcea3197a70c99
Joseph M <jmedina1985@hotmail.com>
2021-07-21 15:22:13 -0700

    Filter categories

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -147,6 +147,11 @@ const Filters = () => {
           </div>
           {/* end of shipping */}
         </form>
+        <button
+          type="button"
+          className="clear-btn"
+          onClick={clearFilters}
+        ></button>
       </div>
     </Wrapper>
   );
