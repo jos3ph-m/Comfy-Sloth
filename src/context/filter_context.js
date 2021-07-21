11b76fa73ec20e7d00252d410366052af6697130
Joseph M <jmedina1985@hotmail.com>
2021-07-21 15:20:56 -0700

    Filter categories

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -70,6 +70,9 @@ export const FilterProvider = ({ children }) => {
     if (name === 'price') {
       value = e.target.dataset.color;
     }
+    if (name === 'shipping') {
+      value = e.target.checked;
+    }
     dispatch({ type: UPDATE_FILTERS, payload: { name, value } });
   };
 
