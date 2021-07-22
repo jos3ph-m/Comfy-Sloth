49aba1252054948e92415d850f1e12ea6bf0fe66
Joseph M <jmedina1985@hotmail.com>
2021-07-22 12:19:20 -0700

    Filter functionality

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -55,7 +55,8 @@ const filter_reducer = (state, action) => {
     return { ...state, filters: { ...state.filters, [name]: value } };
   }
   if (action.type === FILTER_PRODUCTS) {
-    console.log('filtering products');
+    const { all_products } = state;
+    let tempProducts = [...all_products];
     return { ...state };
   }
   if (action.type === CLEAR_FILTERS) {
