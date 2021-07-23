accfc1bfe678ab3ad981002cff6455d91b5ef617
Joseph M <jmedina1985@hotmail.com>
2021-07-23 16:54:37 -0700

    AddToCart setup

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -6,6 +6,7 @@ import { useCartContext } from '../context/cart_context';
 import AmountButtons from './AmountButtons';
 
 const AddToCart = ({ product }) => {
+  const { addToCart } = useCartContext();
   const { id, stock, colors } = product;
 
   const [mainColor, setMainColor] = useState(colors[0]);
