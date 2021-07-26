82c1ab1ab6876d8e7ca2fc3e6ef97b68944ed0f9
Joseph M <jmedina1985@hotmail.com>
2021-07-26 14:50:47 -0700

    Cart page setup

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -11,6 +11,7 @@ import {
 const getLocalStorage = () => {
   let cart = localStorage.getItem('cart');
   if (cart) {
+    return JSON.parse(localStorage.getItem('cart'));
   } else {
     return [];
   }
