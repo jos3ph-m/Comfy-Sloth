b88007774b77bee85ddf72997cebf990cfa8dcd2
Joseph M <jmedina1985@hotmail.com>
2021-07-26 14:55:53 -0700

    Cart page setup

--- src/components/CarContent.js
+++ src/components/CarContent.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import styled from 'styled-components'
-import { useCartContext } from '../context/cart_context'
-import { Link } from 'react-router-dom'
-import CartColumns from './CartColumns'
-import CartItem from './CartItem'
-import CartTotals from './CartTotals'
+import React from 'react';
+import styled from 'styled-components';
+import { useCartContext } from '../context/cart_context';
+import { Link } from 'react-router-dom';
+import CartColumns from './CartColumns';
+import CartItem from './CartItem';
+import CartTotals from './CartTotals';
 
 const CartContent = () => {
-  return <h4>cart content </h4>
-}
+  const { cart, clearCart } = useCartContext();
+  return <Wrapper className="section section-center">cart content </Wrapper>;
+};
 const Wrapper = styled.section`
   .link-container {
     display: flex;
@@ -30,5 +31,5 @@ const Wrapper = styled.section`
   .clear-btn {
     background: var(--clr-black);
   }
-`
-export default CartContent
+`;
+export default CartContent;
