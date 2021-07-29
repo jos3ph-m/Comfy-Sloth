f3671655d8ac4a0aedcdc0663dd1cbc5c1bbe25b
Joseph M <jmedina1985@hotmail.com>
2021-07-29 10:37:48 -0700

    Cart totals

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -73,6 +73,7 @@ const cart_reducer = (state, action) => {
     return { ...state, cart: tempCart };
   }
   if (action.type === COUNT_CART_TOTALS) {
+    const {} = state.cart.reduce(total);
   }
   throw new Error(`No Matching "${action.type}" - action type`);
 };
