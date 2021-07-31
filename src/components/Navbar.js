f819814c3fb93bee0a2869fa7300045ff1814bf4
Joseph M <jmedina1985@hotmail.com>
2021-07-30 18:25:35 -0700

    Auth 0 hide checkout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,6 +10,8 @@ import { useUserContext } from '../context/user_context';
 
 const Nav = () => {
   const { openSidebar } = useProductsContext();
+  const { myUser } = useUserContext();
+
   return (
     <NavContainer>
       <div className="nav-center">
@@ -30,6 +32,11 @@ const Nav = () => {
               </li>
             );
           })}
+          {myUser && (
+            <li>
+              <Link to="/checkout">checkout</Link>
+            </li>
+          )}
         </ul>
         <CartButtons />
       </div>
