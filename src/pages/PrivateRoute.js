7d80ae16f154690c12e1b8c2fd4111154f516171
Joseph M <jmedina1985@hotmail.com>
2021-07-31 19:54:17 -0700

    Auth 0 private route

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -5,6 +5,8 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { useUserContext } from '../context/user_context';
 
 const PrivateRoute = ({ children, ...rest }) => {
+  console.log(children);
+  console.log(rest);
   return <h4>Private Route</h4>;
 };
 export default PrivateRoute;
