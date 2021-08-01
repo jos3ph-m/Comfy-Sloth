61de2356b2739cf72c919944f5c205fc8dac1fae
Joseph M <jmedina1985@hotmail.com>
2021-07-31 19:58:45 -0700

    Auth 0 private route

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -6,7 +6,7 @@ import { useUserContext } from '../context/user_context';
 
 const PrivateRoute = ({ children, ...rest }) => {
   const { myUser } = useUserContext();
-  console.log(myUser);
+
   return (
     <Route
       {...rest}
