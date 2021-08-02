1843c4cdf6cecfda3ffee7cf8c7e7da0788947ec
Joseph M <jmedina1985@hotmail.com>
2021-08-01 19:35:34 -0700

    AuthWrapper

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -5,7 +5,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { useUserContext } from '../context/user_context';
 
 const PrivateRoute = ({ children, ...rest }) => {
-  const { myUser } = useUserContext();
+  const { user } = useAuth0();
 
   return (
     <Route
