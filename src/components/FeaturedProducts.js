ab605f53546e0a987723494cf139689c748e14ca
Joseph M <jmedina1985@hotmail.com>
2021-08-02 14:31:42 -0700

    Fixing warnings

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -29,7 +29,9 @@ const FeaturedProducts = () => {
           return <Product key={product.id} {...product} />;
         })}
       </div>
-      <Link to="/products" className="btn"></Link>
+      <Link to="/products" className="btn">
+        all products
+      </Link>
     </Wrapper>
   );
 };
