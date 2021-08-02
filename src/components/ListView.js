4837f36027c71f4180baea11d6c9720e5ccd0723
Joseph M <jmedina1985@hotmail.com>
2021-08-02 15:12:24 -0700

    Netlify setup

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { formatPrice } from '../utils/helpers';
 import { Link } from 'react-router-dom';
-import products_reducer from '../reducers/products_reducer';
+// import products_reducer from '../reducers/products_reducer';
 const ListView = ({ products }) => {
   return (
     <Wrapper>
