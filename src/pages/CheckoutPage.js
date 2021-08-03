72ae8e2bc6b6efdcf98fa219ee94b3bb1033b5a2
Joseph M <jmedina1985@hotmail.com>
2021-08-03 16:24:44 -0700

    Stripe setup

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -11,18 +11,26 @@ const CheckoutPage = () => {
     <main>
       <PageHero title="checkout" />
       <Wrapper className="page">
-        {cart.length < 1 ? <div className="empty">your cart is empty</div> <Link to="/products" classname='btn'>fill it</Link>: <StripeCheckout />}
-
+        {cart.length < 1 ? (
+          <div className="empty">
+            <h2>your cart is empty</h2>
+            <Link to="/products" classname="btn">
+              fill it
+            </Link>
+          </div>
+        ) : (
+          <StripeCheckout />
+        )}
       </Wrapper>
     </main>
   );
 };
 const Wrapper = styled.div`
-display: flex;
-align-items: center;
-justify-content: center;
-.empty {
-  text-align: center;
-}
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .empty {
+    text-align: center;
+  }
 `;
 export default CheckoutPage;
