dc20094bc0f65fdb775a6b49d5fa7f9b0ad4a703
Joseph M <jmedina1985@hotmail.com>
2021-08-04 19:50:03 -0700

    Stripe setup

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -1,12 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { loadStripe } from '@stripe/stripe-js';
-import {
-  CardElement,
-  useStripe,
-  Elements,
-  useElements,
-} from '@stripe/react-stripe-js';
+
 import axios from 'axios';
 import { useCartContext } from '../context/cart_context';
 import { useUserContext } from '../context/user_context';
