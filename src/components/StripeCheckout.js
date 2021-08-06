284ac3c116124ec8f2d3c522c7e0fcc3f1c5de78
Joseph M <jmedina1985@hotmail.com>
2021-08-06 15:09:57 -0700

    Stripe setup

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -80,6 +80,10 @@ const CheckoutForm = () => {
         {/* Show success message */}
         <p className={succeeded ? 'result-message' : 'result-message hidden'}>
           Payment succeeded, see the result in your
+          <a href={`https://dashboard.stripe.com/test/payments`}>
+            Stripe dashboard.
+          </a>
+          Refresh the page to pay again
         </p>
       </form>
     </div>
