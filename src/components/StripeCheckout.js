77e59d8b4997d4a99563e7d50967775d53d013be
Joseph M <jmedina1985@hotmail.com>
2021-08-07 17:21:09 -0700

    Stripe setup

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -49,7 +49,8 @@ const CheckoutForm = () => {
   const createPaymentIntent = async () => {
     try {
       const data = await axios.post(
-        '/.netlify/functions/create-payment-intent'
+        '/.netlify/functions/create-payment-intent',
+        JSON.stringify({ cart, shipping_fee, total_amount })
       );
     } catch (error) {}
   };
