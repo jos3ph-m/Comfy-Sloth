e3fb20ef998001d88164814b42d1e48b61b7f07a
Joseph M <jmedina1985@hotmail.com>
2021-08-07 17:14:33 -0700

    Stripe setup

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -1,6 +1,7 @@
 // domain/.netlify/functions/create-payment-intent
 
 exports.handler = async function (event, context) {
+  console.log(event);
   return {
     statusCode: 200,
     body: 'hello world',
