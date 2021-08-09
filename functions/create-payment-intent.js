ed1ed0600ebfe74bbb767a5dcf3ec15cddb803d7
Joseph M <jmedina1985@hotmail.com>
2021-08-08 19:46:25 -0700

    Stripe setup

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -1,6 +1,8 @@
 // domain/.netlify/functions/create-payment-intent
 require('dotenv').config();
 
+const stripe = require('stripe')(process.env.REACT_APP_STRIPE_SECRET_KEY);
+
 exports.handler = async function (event, context) {
   if (event.body) {
     const { cart, shipping_fee, total_amount } = JSON.parse(event.body);
